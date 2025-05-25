8f0b75748a68af1c3a0ca6d3f918572c7f1ce8d2
Abhishek-raj093 <ss0986825@gmail.com>
2025-05-25 13:15:19 +0530

    Created All the API

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { checkAvailabilityAPI, createBooking, getHotelBookings, getUserBookings } from "../controllers/bookingController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const bookingRoute = express.Router();
+
+bookingRoute.post('/check-availability', checkAvailabilityAPI);
+bookingRoute.post('/book', protect, createBooking);
+bookingRoute.get('/user', protect, getUserBookings);
+bookingRoute.get('/hotel', protect, getHotelBookings);
+
+export default bookingRoute;
